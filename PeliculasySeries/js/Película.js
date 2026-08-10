4be5012114a5dfda553f73fbb923b4bf6f498b0c
Synkro-App <Synkro.1922@gmail.com>
2026-08-10 13:41:50 +0200

    Update Película.js

--- PeliculasySeries/js/Película.js
+++ PeliculasySeries/js/Película.js
@@ -263,7 +263,7 @@ async function cargarDatosPelicula() {
                     btnCat.className = "btn-red-item";
                     btnCat.textContent = cat;
                     btnCat.addEventListener("click", () => {
-                        localStorage.setItem("categoriaselecciona", cat);
+                        localStorage.setItem("categoriaseleccionada", cat);
                         window.location.href = "Categorías.html";
                     });
                     valCategorias.appendChild(btnCat);
